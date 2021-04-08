32bfec4e094be32b320a921c64e960d287b0c8b8
Will Lane <williamlane923@gmail.com>
2021-04-08 20:31:15 +0200

    removed proc macros in favor of traits

--- dogehouse-macros/src/lib.rs
+++
@@ -1,9 +0,0 @@
-extern crate proc_macro;
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
-  println!("attr: \"{}\"", attr.to_string());
-  println!("item: \"{}\"", item.to_string());
-  item
-}
--- dogehouse/src/config.rs
+++
@@ -1,2 +0,0 @@
-pub static API_URL: &'static str = "wss://api.dogehouse.tv/socket";
-pub static HEART_BEAT_INTERVAL: u8 = 8;
