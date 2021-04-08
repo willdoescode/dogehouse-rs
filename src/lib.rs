b7c67dd28ebe8b7070b25f459224ee9842f60244
Will Lane <williamlane923@gmail.com>
2021-04-08 22:57:33 +0200

    started on client + handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub(crate) trait Handler {}
+
+pub(crate) struct Client<'t, T> where T: Handler {
+	token: &'t String,
+	refresh_token: &'t String,
+	handler: Option<T>,
 }
+
+impl<'t, T> Client<'t, T> where T: Handler {
+	pub(crate) fn new(token: &'t String, refresh_token: &'t String) -> Self {
+		Self {
+			token,
+			refresh_token,
+			handler: None,
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests;
+
