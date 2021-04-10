48c8c4b4927320b1a3254dd672389af0bc6e36c4
Will Lane <williamlane923@gmail.com>
2021-04-09 20:19:36 -0700

    updated to Message

--- test_dogehouse-rs/src/main.rs
+++ test_dogehouse-rs/src/main.rs
@@ -1,11 +1,12 @@
 use dogehouse_rs::prelude::*;
+use dogehouse_rs::message::Message;
 
 struct Handle;
 
 impl Handler for Handle {
   fn on_ready(&self, user: String) { println!("{}", user); }
 
-  fn on_message(&self, msg: String) { println!("{}", msg); }
+  fn on_message(&self, msg: &Message) { println!("{:?}", msg); }
 }
 
 fn main() {
